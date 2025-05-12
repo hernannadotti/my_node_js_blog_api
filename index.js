cdadc70ccf89bbe904843118f5420bc1d1158927
hernannadotti <hernan.leandro10@gmail.com>
2025-05-12 00:32:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./db');
+const postsRouter = require('./routes/posts');
+
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// Init Middleware
+app.use(cors());
+app.use(express.json());
+
+// Define Routes
+app.use('/api/posts', postsRouter);
+
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ routes/posts.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
